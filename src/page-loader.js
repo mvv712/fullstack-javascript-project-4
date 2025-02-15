580f64a345b895d806d296f4a78124bb7788b0a1
mvv712 <mvv712@mail.ru>
2025-02-15 15:27:23 +0300

    fix linter check

--- src/page-loader.js
+++ src/page-loader.js
@@ -4,19 +4,18 @@ import path from 'path';
 
 const urlToFileName = (url) => url.split('://')[1].replace(/[^\w]/g, '-').concat('.html');
 
-// const pageLoader = (urlName, output = '') => {
-//   let url;
-//   try {
-//     url = new URL(urlName);
-//   } catch (err) {
-//     throw new Error('wrong URL string');
-//   }
+const pageLoader = (urlName, output = '') => {
+  let url;
+  try {
+    url = new URL(urlName);
+  } catch (err) {
+    throw new Error('wrong URL string');
+  }
 
-//   const urlSavePath = path.join(output.output, urlToFileName(urlName));
-//   return axios.get(urlName)
-//     .then(({ data }) => writeFile(urlSavePath, data))
-//     .then(() => urlSavePath);
-// };
+  const urlSavePath = path.join(output.output, urlToFileName(urlName));
+  return axios.get(urlName)
+    .then(({ data }) => writeFile(urlSavePath, data))
+    .then(() => urlSavePath);
+};
 
-// export default pageLoader;
-export default urlToFileName;
+export default pageLoader;
