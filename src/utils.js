ebe15d5cef626165ac8851845423ff2ed0334e1c
mvv712 <mvv712@mail.ru>
2025-03-16 17:19:56 +0300

    urlToFileName fixed

--- src/utils.js
+++ src/utils.js
@@ -4,19 +4,30 @@ const getFixturePath = (filename = '') => path.join('__fixtures__', filename);
 
 const urlToFileName = (url) => {
   const fileExts = ['png', 'jpeg', 'jpg', 'css', 'js'];
+  const getFileExtension = (fileName) => {
+    const match = fileName.match(/\.([a-zA-Z0-9]+)$/);
+    return match ? match[1] : null;
+  }
 
-  const host = url.split('://').length === 2 ? url.split('://')[1] : url.split('://')[0];
-  const hostElems = host.split('.');
-  const ext = hostElems.pop();
+  let host = url.replace(/(^\w+:\/\/)/, '');
+  const ext = getFileExtension(host);
+
+  if (host[host.length - 1].match(/[^\w]/g)) {
+    host = host.slice(0, -1);
+  }
 
   if (!fileExts.includes(ext)) {
     return host.replace(/[^\w]/g, '-').concat('.html');
   }
-  return hostElems.join('.').replace(/[^\w]/g, '-').concat(`.${ext}`);
+
+  return host.replace(/(\.[a-zA-Z0-9]+)$/, '').replace(/[^\w]/g, '-').concat(`.${ext}`);
 };
 
 const urlToFolderName = (url) => {
-  const host = url.split('://').length === 2 ? url.split('://')[1] : url.split('://')[0];
+  let host = url.replace(/(^\w+:\/\/)/, '');
+  if (host[host.length - 1].match(/[^\w]/g)) {
+    host = host.slice(0, -1);
+  }
   return host.replace(/[^\w]/g, '-').concat('_files');
 };
 
