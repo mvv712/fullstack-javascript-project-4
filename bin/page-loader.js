3cdffcf3a5399d0b6f48bf67fbf07ff474cf8b42
Maksimova Vera <mvv712@mail.ru>
2025-03-24 21:48:17 +0300

    arguments fixed

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -8,7 +8,7 @@ program
   .description('Page loader utility')
   .version('1.0.0')
   .option('-o, --output <path>', 'output directory', process.cwd())
-  .argument('<url>', 'URL to load')
+  .arguments('<url>', 'URL to load')
   .action((url) => {
     const { output } = program.opts();
 
