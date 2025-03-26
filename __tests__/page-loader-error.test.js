74efcbffb5ffb16703bacbfaf525c717038ba071
Maksimova Vera <mvv712@mail.ru>
2025-03-26 19:55:32 +0300

    jest module added

--- __tests__/page-loader-error.test.js
+++ __tests__/page-loader-error.test.js
@@ -2,6 +2,7 @@ import fsp from 'fs/promises';
 import path from 'path';
 import nock from 'nock';
 import os from 'os';
+import { afterEach, beforeEach, expect, test } from '@jest/globals';
 import loadPage from '../src/page-loader.js';
 import { getFixturePath } from '../src/utils.js';
 
@@ -15,35 +16,33 @@ const testUrl = getFixturePath('input/page-loader-test.html');
 let testUrlContent;
 let testOutput;
 
-describe('page-loader error', () => {
-  beforeEach(async () => {
-    testUrlContent = await fsp.readFile(testUrl, 'utf-8');
-    testOutput = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  });
+beforeEach(async () => {
+  testUrlContent = await fsp.readFile(testUrl, 'utf-8');
+  testOutput = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
 
-  test('wrong url', async () => {
-    await expect(loadPage('notUrl', testOutput)).rejects.toThrow('wrong URL string');
-  });
+test('wrong url', async () => {
+  await expect(loadPage('notUrl', testOutput)).rejects.toThrow('wrong URL string');
+});
 
-  test('network error', async () => {
-    nock(url.host)
-      .get(url.page)
-      .reply(404, 'Page not found');
+test('network error', async () => {
+  nock(url.host)
+    .get(url.page)
+    .reply(404, 'Page not found');
 
-    await expect(loadPage(url.full, testOutput)).rejects.toThrow('Request failed with status code 404');
-  });
+  await expect(loadPage(url.full, testOutput)).rejects.toThrow('Request failed with status code 404');
+});
 
-  test('permissions error in folder', async () => {
-    nock(url.host)
-      .get(url.page)
-      .reply(200, testUrlContent);
+test('permissions error in folder', async () => {
+  nock(url.host)
+    .get(url.page)
+    .reply(200, testUrlContent);
 
-    const invalidPath = path.join(os.tmpdir(), 'invalid-directory');
-    await expect(loadPage(url.full, invalidPath)).rejects.toThrow(`Not enough permissions in this folder ${invalidPath}`);
-  });
+  const invalidPath = path.join(os.tmpdir(), 'invalid-directory');
+  await expect(loadPage(url.full, invalidPath)).rejects.toThrow(`Not enough permissions in this folder ${invalidPath}`);
+});
 
-  afterEach(async () => {
-    nock.cleanAll();
-    await fsp.rmdir(testOutput, { recursive: true });
-  });
+afterEach(async () => {
+  nock.cleanAll();
+  await fsp.rmdir(testOutput, { recursive: true });
 });
--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -2,6 +2,7 @@ import fsp from 'fs/promises';
 import path from 'path';
 import nock from 'nock';
 import os from 'os';
+import { afterEach, beforeEach, expect, test } from '@jest/globals';
 import loadPage from '../src/page-loader.js';
 import { getFixturePath, urlToFileName } from '../src/utils.js';
 
@@ -17,49 +18,47 @@ let testUrlContent;
 let testOutput;
 let testUrlPath;
 
-describe('page-loader', () => {
-  beforeEach(async () => {
-    nock(url.host)
-      .get(url.page)
-      .reply(200, testUrlContent)
-      .get(url.img)
-      .replyWithFile(200, getFixturePath('input/ru-hexlet-io-assets-professions-nodejs.png'))
-      .get('/assets/application.css')
-      .replyWithFile(200, getFixturePath('input/application.css'))
-      .get('/courses')
-      .replyWithFile(200, getFixturePath('input/ru-hexlet-io-courses.html'))
-      .get('/packs/js/runtime.js')
-      .replyWithFile(200, getFixturePath('input/ru-hexlet-io-packs-js-runtime.js'))
-      .get('/assets/application2.css')
-      .replyWithFile(200, getFixturePath('input/application2.css'));
+beforeEach(async () => {
+  nock(url.host)
+    .get(url.page)
+    .reply(200, testUrlContent)
+    .get(url.img)
+    .replyWithFile(200, getFixturePath('input/ru-hexlet-io-assets-professions-nodejs.png'))
+    .get('/assets/application.css')
+    .replyWithFile(200, getFixturePath('input/application.css'))
+    .get('/courses')
+    .replyWithFile(200, getFixturePath('input/ru-hexlet-io-courses.html'))
+    .get('/packs/js/runtime.js')
+    .replyWithFile(200, getFixturePath('input/ru-hexlet-io-packs-js-runtime.js'))
+    .get('/assets/application2.css')
+    .replyWithFile(200, getFixturePath('input/application2.css'));
 
-    testUrlContent = await fsp.readFile(testUrl, 'utf-8');
-    testOutput = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-    testUrlPath = path.join(testOutput, urlToFileName(url.full));
-  });
+  testUrlContent = await fsp.readFile(testUrl, 'utf-8');
+  testOutput = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  testUrlPath = path.join(testOutput, urlToFileName(url.full));
+});
 
-  test('return path test', async () => {
-    const received = await loadPage(url.full, testOutput);
-    expect(received).toBe(testUrlPath);
-  });
+test('return path test', async () => {
+  const received = await loadPage(url.full, testOutput);
+  expect(received).toBe(testUrlPath);
+});
 
-  test('correct result', async () => {
-    const receivedPath = await loadPage(url.full, testOutput);
-    const receivedContent = await fsp.readFile(receivedPath, 'utf-8');
-    const expectedContent = await fsp.readFile(getFixturePath('result.html'), 'utf-8');
-    expect(receivedContent).toStrictEqual(expectedContent);
-  });
+test('correct result', async () => {
+  const receivedPath = await loadPage(url.full, testOutput);
+  const receivedContent = await fsp.readFile(receivedPath, 'utf-8');
+  const expectedContent = await fsp.readFile(getFixturePath('result.html'), 'utf-8');
+  expect(receivedContent).toStrictEqual(expectedContent);
+});
 
-  test('load imgs', async () => {
-    await loadPage(url.full, testOutput);
-    const img = (await fsp.readFile(path.join(testOutput, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-professions-nodejs.png'), 'utf-8')).toString();
-    const testImg = (await fsp.readFile(getFixturePath('input/ru-hexlet-io-assets-professions-nodejs.png'), 'utf-8')).toString();
+test('load imgs', async () => {
+  await loadPage(url.full, testOutput);
+  const img = (await fsp.readFile(path.join(testOutput, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-professions-nodejs.png'), 'utf-8')).toString();
+  const testImg = (await fsp.readFile(getFixturePath('input/ru-hexlet-io-assets-professions-nodejs.png'), 'utf-8')).toString();
 
-    expect(img).toStrictEqual(testImg);
-  });
+  expect(img).toStrictEqual(testImg);
+});
 
-  afterEach(async () => {
-    nock.cleanAll();
-    await fsp.rmdir(testOutput, { recursive: true });
-  });
+afterEach(async () => {
+  nock.cleanAll();
+  await fsp.rmdir(testOutput, { recursive: true });
 });
