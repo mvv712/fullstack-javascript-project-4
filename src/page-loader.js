a9c4e7fac0426a28a8332fbada13cff1e718904e
Maksimova Vera <mvv712@mail.ru>
2025-03-26 19:28:12 +0300

    debug added

--- src/page-loader.js
+++ src/page-loader.js
@@ -3,6 +3,7 @@ import fsp from 'fs/promises';
 import path from 'path';
 import { urlToFileName } from './utils.js';
 import loadFiles from './file-loader.js';
+import debug from './debug.js';
 
 const pageLoader = async (href, output = '') => {
   let url;
@@ -14,12 +15,14 @@ const pageLoader = async (href, output = '') => {
 
   const urlPath = path.join(output, urlToFileName(url.href));
 
+  debug('check access folder %s', output);
   try {
     await fsp.access(output);
   } catch (err) {
     throw new Error(`Not enough permissions in this folder ${output}`);
   }
 
+  debug('start load %s', url.href);
   return axios.get(url.href)
     .then(({ data }) => loadFiles(data, url, output))
     .then((html) => fsp.writeFile(urlPath, html))
